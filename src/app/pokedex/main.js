c66c98b8b01f934ca7f8d77c63e0b04e838101c3
eduardo <24103009@alumnos.univo.edu.mx>
2025-04-28 00:50:35 -0600

    pokedex

--- src/app/pokedex/main.js
+++ src/app/pokedex/main.js
@@ -20,10 +20,17 @@ function mostrarpokemon(datospokemon, ContenedorNumero){
     <h2 class="pk-name" >${datospokemon.name.toUpperCase()}</h2>
     <img class="pk-img" src="${datospokemon.sprites.other["official-artwork"].front_default}">
     <p>Numero:${datospokemon.id}</p>
-    <p>Weight:${datospokemon.weight/10}kg</p>
+    <p>weight:${datospokemon.weight/10}kg</p>
     <p>height:${datospokemon.height}m</p>
+    '
+}
+//error en busqueda de pokemon xddddddd
+function mostrarerror(ContenedorNumero){
+    let infoDivid = ' pokemoninfo${ContenedorNumero}'; 
+    let infoDiv = document.getElementById(infoDivid);
+    infoDivid.innerHTML = '
 
-    
+    <p class="pk-ms">pokemon no encontrado. <br> intenta con otro nombre o numero</p>
     
     '
 }
